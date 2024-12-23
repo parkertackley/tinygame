4e883a4a10c17e85f96c89142d5c278bf92d69f4
parkertackley <parkertackley@gmail.com>
2024-12-23 17:59:17 -0500

    first raycast/virtual rangefinder

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ int main() {
                        "0              0"\
                        "0000222222220000";
     assert(sizeof(map) == map_w * map_h + 1); // assert the map size is == to the calculation, +1 for null terminator in a string
-    float player_x = 3.4556, player_y = 2.345;
+    float player_x = 3.4556, player_y = 2.345, player_a = 1.523;
 
     // fill .ppm with color gradient
     for(size_t j = 0; j < win_h; ++j) {
@@ -92,6 +92,19 @@ int main() {
     }
 
     draw_rectangle(framebuffer, win_w, win_h, player_x * rect_w, player_y * rect_h, 5, 5, pack_color(255, 255, 255));
+
+    for(float t = 0; t < 20; t += 0.05) {
+        float cx = player_x + t * cos(player_a);
+        float cy = player_y + t * sin(player_a);
+
+        if(map[int(cx) + int(cy) * map_w] != ' ')
+            break;
+
+        size_t pix_x = cx * rect_w;
+        size_t pix_y = cy * rect_h;
+        framebuffer[pix_x + pix_y * win_w] = pack_color(255, 255, 255);
+
+    }
 
     drop_ppm_image("./out.ppm", framebuffer, win_w, win_h);
 
