098e82511336fc8f734dd4e8584d50d0781741ee
parkertackley <parkertackley@gmail.com>
2024-12-25 14:11:44 -0500

    3d rendering without fish eye. running will create many .ppm as an animation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cstdint>
 #include <cassert>
+#include <sstream>
+#include <iomanip>
 
 uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255) {
     return (a << 24) + (b << 16) + (g << 8) + r;
@@ -75,39 +77,58 @@ int main() {
     float player_a = 1.523;
     const float fov = M_PI / 3.0;
 
+    const size_t ncolors = 10;
+    std::vector<uint32_t> colors(ncolors);
+    for(size_t i = 0; i < ncolors; ++i) {
+        colors[i] = pack_color(rand()%255, rand()%255, rand()%255);
+    }
+
     // draws map
     const size_t rect_w = win_w / (map_w * 2);
     const size_t rect_h = win_h / map_h;
-    for(size_t j = 0; j < map_h; ++j) {
-        for(size_t i = 0; i < map_w; ++i) {
-            if(map[i + j * map_w] == ' ') // skip empty spots
-                continue;
-            size_t rect_x = i * rect_w;
-            size_t rect_y = j * rect_h;
-            draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h, pack_color(0, 255, 255));
-        }
-    }
+    
+    for(size_t frame = 0; frame < 360; ++frame) {
+        std::stringstream ss;
+        ss << std::setfill('0') << std::setw(5) << frame << ".ppm";
+        player_a += 2 * M_PI / 360;
 
-    // raycasting loop
-    for(size_t i = 0; i < win_w / 2; ++i) {
-        float angle = player_a - fov / 2 + fov * i / float(win_w / 2);
+        framebuffer = std::vector<uint32_t>(win_w * win_h, pack_color(255, 255, 255));
 
-        for(float t = 0; t < 20; t += 0.05) {
-            float cx = player_x + t * cos(angle);
-            float cy = player_y + t * sin(angle);
-
-            size_t pix_x = cx * rect_w;
-            size_t pix_y = cy * rect_h;
-            framebuffer[pix_x + pix_y * win_w] = pack_color(160, 160, 160);
-
-            if(map[int(cx) + int(cy) * map_w] != ' ') {
-                size_t column_height = win_h / t;
-                draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2, 1, column_height, pack_color(0, 255, 255));
-                break;
+        for(size_t j = 0; j < map_h; ++j) {
+            for(size_t i = 0; i < map_w; ++i) {
+                if(map[i + j * map_w] == ' ')
+                    continue;
+                size_t rect_x = i * rect_w;
+                size_t rect_y = j * rect_h;
+                size_t icolor = map[i + j * map_w] - '0';
+                assert(icolor < ncolors);
+                draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h, colors[icolor]);
             }
-
         }
 
+        // raycasting loop
+        for(size_t i = 0; i < win_w / 2; ++i) {
+            float angle = player_a - fov / 2 + fov * i / float(win_w / 2);
+
+            for(float t = 0; t < 20; t += 0.01) {
+                float cx = player_x + t * cos(angle);
+                float cy = player_y + t * sin(angle);
+
+                size_t pix_x = cx * rect_w;
+                size_t pix_y = cy * rect_h;
+                framebuffer[pix_x + pix_y * win_w] = pack_color(160, 160, 160);
+
+                if(map[int(cx) + int(cy) * map_w] != ' ') {
+                    size_t icolor = map[int(cx) + int(cy) * map_w] - '0';
+                    assert(icolor < ncolors);
+                    size_t column_height = win_h / (t * cos(angle - player_a));
+                    draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2, 1, column_height, pack_color(0, 255, 255));
+                    break;
+                }
+
+            }
+            drop_ppm_image(ss.str(), framebuffer, win_w, win_h);
+        }
     }
 
     drop_ppm_image("./out.ppm", framebuffer, win_w, win_h);
