9a78226cdd90dd8cec6d0196f9a050d6df72367a
parkertackley <parkertackley@gmail.com>
2024-12-24 21:29:43 -0500

    3d view of 1 frame, cleaned up code

--- main.cpp
+++ main.cpp
@@ -37,15 +37,17 @@ void draw_rectangle(std::vector<uint32_t> &img, const size_t img_w, const size_t
         for(size_t j = 0; j < h; ++j) {
             size_t cx = x + i;
             size_t cy = y + j;
-            assert(cx < img_w && cy < img_h);
+            if(cx >= img_w || cy >= img_h)
+                continue;
             img[cx + cy * img_w] = color;
         }
     }
 }
 
 int main() {
-    const size_t win_w = 512, win_h = 512;
-    std::vector<uint32_t> framebuffer(win_w * win_h, 255);
+    const size_t win_w = 1024;
+    const size_t win_h = 512;
+    std::vector<uint32_t> framebuffer(win_w * win_h, pack_color(255, 255, 255));
 
     // setting the map boundaries 
     const size_t map_w = 16, map_h = 16;
@@ -68,21 +70,13 @@ int main() {
     assert(sizeof(map) == map_w * map_h + 1); // assert the map size is == to the calculation, +1 for null terminator in a string
 
     // player details
-    float player_x = 3.4556, player_y = 2.345, player_a = 1.523;
+    float player_x = 3.4556;
+    float player_y = 2.345;
+    float player_a = 1.523;
     const float fov = M_PI / 3.0;
 
-    // fill .ppm with color gradient
-    for(size_t j = 0; j < win_h; ++j) {
-        for(size_t i = 0; i < win_w; ++i) {
-            uint8_t r = 255 * j / float(win_h);
-            uint8_t g = 255 * i / float(win_w);
-            uint8_t b = 0;
-            framebuffer[i + j * win_w] = pack_color(r, g, b);
-        }
-    }
-
     // draws map
-    const size_t rect_w = win_w / map_w;
+    const size_t rect_w = win_w / (map_w * 2);
     const size_t rect_h = win_h / map_h;
     for(size_t j = 0; j < map_h; ++j) {
         for(size_t i = 0; i < map_w; ++i) {
@@ -94,22 +88,23 @@ int main() {
         }
     }
 
-    draw_rectangle(framebuffer, win_w, win_h, player_x * rect_w, player_y * rect_h, 5, 5, pack_color(255, 255, 255));
-
     // raycasting loop
-    for(size_t i = 0; i < win_w; ++i) {
-        float angle = player_a - fov / 2 + fov * i / float(win_w);
+    for(size_t i = 0; i < win_w / 2; ++i) {
+        float angle = player_a - fov / 2 + fov * i / float(win_w / 2);
 
         for(float t = 0; t < 20; t += 0.05) {
             float cx = player_x + t * cos(angle);
             float cy = player_y + t * sin(angle);
 
-            if(map[int(cx) + int(cy) * map_w] != ' ')
-                break;
-
             size_t pix_x = cx * rect_w;
             size_t pix_y = cy * rect_h;
-            framebuffer[pix_x + pix_y * win_w] = pack_color(255, 255, 255);
+            framebuffer[pix_x + pix_y * win_w] = pack_color(160, 160, 160);
+
+            if(map[int(cx) + int(cy) * map_w] != ' ') {
+                size_t column_height = win_h / t;
+                draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2, 1, column_height, pack_color(0, 255, 255));
+                break;
+            }
 
         }
 
