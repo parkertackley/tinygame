4d8eba1c4b7686ff4cf89441290b8eec0657a327
parkertackley <parkertackley@gmail.com>
2024-12-26 13:08:36 -0500

    bug: walls wont color correctly

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <cstdint>
 #include <cassert>
-#define STD_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255) {
@@ -14,12 +14,13 @@ bool load_texture(const std::string filename, std::vector<uint32_t> &texture, si
     int nchannels = -1, w, h;
     unsigned char *pixmap = stbi_load(filename.c_str(), &w, &h, &nchannels, 0);
     if(!pixmap) {
-        std::cerr << "Error: can not load the textures!" <<std::endl;
+        std::cerr << "Error: can not load the textures!" << std::endl;
         return false;
     }
 
     if(4 != nchannels) {
         std::cerr << "Error: the texture must be a 32 bit image!" << std::endl;
+        stbi_image_free(pixmap);
         return false;
     }
 
@@ -57,7 +58,7 @@ void unpack_color(const uint32_t &color, uint8_t &r, uint8_t &g, uint8_t &b, uin
 void drop_ppm_image(const std::string filename, const std::vector<uint32_t> &image, const size_t w, const  size_t h) {
     assert(image.size() == w * h);
 
-    std::ofstream ofs(filename, std::ios::binary);
+    std::ofstream ofs(filename);
     ofs << "P6\n" << w << " " << h << "\n255\n";
 
     for(size_t i = 0; i < h * w; ++i) {
@@ -89,7 +90,8 @@ int main() {
     std::vector<uint32_t> framebuffer(win_w * win_h, pack_color(255, 255, 255));
 
     // setting the map boundaries 
-    const size_t map_w = 16, map_h = 16;
+    const size_t map_w = 16;
+    const size_t map_h = 16;
     const char map[] = "0000222222220000"\
                        "0              0"\
                        "0      11111   0"\
@@ -97,15 +99,16 @@ int main() {
                        "0     0  1110000"\
                        "0     3        0"\
                        "0   10000      0"\
-                       "0   0   11100  0"\
-                       "0   0   0      0"\
-                       "0   0   1  00000"\
+                       "0   3   11100  0"\
+                       "5   4   0      0"\
+                       "5   4   1  00000"\
                        "0       1      0"\
                        "0       1      0"\
                        "0       0      0"\
                        "0 0000000      0"\
                        "0              0"\
                        "0000222222220000";
+
     assert(sizeof(map) == map_w * map_h + 1); // assert the map size is == to the calculation, +1 for null terminator in a string
 
     // player details
@@ -114,16 +117,10 @@ int main() {
     float player_a = 1.523;
     const float fov = M_PI / 3.0;
 
-    const size_t ncolors = 10;
-    std::vector<uint32_t> colors(ncolors);
-    for(size_t i = 0; i < ncolors; ++i) {
-        colors[i] = pack_color(rand()%255, rand()%255, rand()%255);
-    }
-
-    std::vector<uint32_t> walltext; // texture for walls
-    size_t walltext_size; // texture dimesnsions
-    size_t walltext_cnt;
-    if(!load_texture("../walltext.png", walltext, walltext_size, walltext_cnt)) {
+    std::vector<uint32_t> walltext;     // texture for walls
+    size_t walltext_size;               // texture dimesnsions
+    size_t walltext_cnt;                // number of different textures in image
+    if(!load_texture("walltext.png", walltext, walltext_size, walltext_cnt)) {
         std::cerr << "Failed to load wall textures" << std::endl;
         return -1;
     }
@@ -131,23 +128,21 @@ int main() {
     // draws map
     const size_t rect_w = win_w / (map_w * 2);
     const size_t rect_h = win_h / map_h;
-
     for(size_t j = 0; j < map_h; ++j) {
         for(size_t i = 0; i < map_w; ++i) {
             if(map[i + j * map_w] == ' ')
                 continue;
             size_t rect_x = i * rect_w;
             size_t rect_y = j * rect_h;
-            size_t icolor = map[i + j * map_w] - '0';
-            assert(icolor < ncolors);
-            draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h, colors[icolor]);
+            size_t textid = map[i + j * map_w] - '0';
+            assert(textid < walltext_cnt);
+            draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h, walltext[textid * walltext_size]);
         }
     }
 
-        // raycasting loop
+    // raycasting loop
     for(size_t i = 0; i < win_w / 2; ++i) {
         float angle = player_a - fov / 2 + fov * i / float(win_w / 2);
-
         for(float t = 0; t < 20; t += 0.01) {
             float cx = player_x + t * cos(angle);
             float cy = player_y + t * sin(angle);
@@ -157,17 +152,11 @@ int main() {
             framebuffer[pix_x + pix_y * win_w] = pack_color(160, 160, 160);
 
             if(map[int(cx) + int(cy) * map_w] != ' ') {
-                size_t icolor = map[int(cx) + int(cy) * map_w] - '0';
-                assert(icolor < ncolors);
+                size_t textid = map[int(cx) + int(cy) * map_w] - '0';
+                assert(textid < walltext_cnt);
                 size_t column_height = win_h / (t * cos(angle - player_a)); // removes fish eye
-                draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2, 1, column_height, pack_color(0, 255, 255));
+                draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2, 1, column_height, walltext[textid * walltext_size]);
                 break;
-            }
-            const size_t textid = 4;
-            for(size_t i = 0; i < walltext_size; ++i) {
-                for(size_t j = 0; j < walltext_size; ++j) {
-                    framebuffer[i + j * win_w] = walltext[i + textid * walltext_size + j * walltext_size * walltext_cnt];
-                }
             }
         }
     }
