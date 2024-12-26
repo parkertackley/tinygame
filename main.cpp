2471d691cb800513b3ccfef9cc67f8fc9df86939
parkertackley <parkertackley@gmail.com>
2024-12-25 19:10:34 -0500

    bug: not working on mac

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 #include <iostream>
 #include <cstdint>
 #include <cassert>
-#include "stb_image.h"
-
 #define STD_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255) {
     return (a << 24) + (b << 16) + (g << 8) + r;
